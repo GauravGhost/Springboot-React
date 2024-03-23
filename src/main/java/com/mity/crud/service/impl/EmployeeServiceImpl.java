f174b5256e66c61fcda6370910cab6a9f03b3ee0
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-03-23 15:24:11 +0530

    Added Get, Get all and create Employee api

---
+++ src/main/java/com/mity/crud/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mity.crud.service.impl;
+
+import com.mity.crud.dto.EmployeeDto;
+import com.mity.crud.entity.Employee;
+import com.mity.crud.exception.ResourceNotFoundException;
+import com.mity.crud.mapper.EmployeeMapper;
+import com.mity.crud.repository.EmployeeRepository;
+import com.mity.crud.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with the given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::mapToEmployeeDto)
+                .collect(Collectors.toList());
+    }
+}
