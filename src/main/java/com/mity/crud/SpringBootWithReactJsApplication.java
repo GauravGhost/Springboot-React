2775792548fff60bc778d38f1ef268cecbb0a704
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-03-23 13:14:04 +0530

    Base Setup

---
+++ src/main/java/com/mity/crud/SpringBootWithReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.mity.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithReactJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithReactJsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mity/crud/SpringBootWithReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mity.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactJsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
